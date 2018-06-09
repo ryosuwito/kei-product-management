1b2f59c793db502d9cd337b1b3a600dd9bb9a2f9
kemong <kemong.dev123@gmail.com>
2018-06-10 04:54:33 +0700

    adding catalog admin class

--- catalog/models.py
+++ catalog/models.py
@@ -1,19 +1,43 @@
 from django.db import models
+from django_extensions.db.fields import AutoSlugField
 
 class Category(models.Model):
-    name = models.CharField(max_length = 100)
-    slug = models.SlugField(max_length= 100)
-    description = models.TextField(blank=True)
-    is_archived = models.BooleanField(default = False) 
+    name = models.CharField(max_length = 100,
+            help_text="Nama Kategori")
+    slug = AutoSlugField(max_length=100, 
+            unique=True, 
+            populate_from=('name',))
+    description = models.TextField(blank=True,
+            help_text="Deskripsi Kategori")
+    is_archived = models.BooleanField(default = False,
+            help_text="Centang untuk Menyembunyikan Kategori") 
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+       return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length = 200)
-    slug = models.SlugField(max_length = 150)
-    description = models.TextField()
-    photo = models.ImageField(upload_to = 'product_photo')
-    price = models.IntegerField()
-    unit_weight = models.IntegerField()
-    product_available = models.BooleanField(default = True)
-    is_archived = models.BooleanField(default = False)
-    categories = models.ManyToManyField(Category, related_name="category")
+    name = models.CharField(max_length = 200,
+            help_text="Nama Produk")
+    slug = AutoSlugField(max_length=100, unique=True, populate_from=('name',))
+    description = models.TextField(help_text="Deskripsi Produk")
+    photo = models.ImageField(upload_to = 'product_photo',
+            help_text="Foto Produk")
+    price = models.IntegerField(help_text="Harga Produk")
+    unit_weight = models.IntegerField(help_text="Berat Satuan Produk dalam gram")
+    product_available = models.BooleanField(default = True,
+            help_text="Centang Jika Produk Tersedia")
+    is_archived = models.BooleanField(default = False,
+            help_text="Centang untuk Menyembunyikan Produk")
+    categories = models.ManyToManyField(Category, 
+            related_name="category",
+            help_text="Kategori Produk")
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+       return self.name
     
