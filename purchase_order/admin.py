5872652c90ebed3d3a3bb619b2f36c73ae057e16
ryosuwito <ryosuwito.grades@gmail.com>
2018-10-04 22:29:24 +0700

    add_is_featured_to_product

--- purchase_order/admin.py
+++ purchase_order/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import PurchaseOrder
+from .models import PurchaseOrder, PurchaseOrderItem
 
 class PurchaseOrderAdmin(admin.ModelAdmin):
     model = PurchaseOrder
     
 admin.site.register(PurchaseOrder, PurchaseOrderAdmin)
+
+class PurchaseOrderItemAdmin(admin.ModelAdmin):
+    model = PurchaseOrderItem
+    
+admin.site.register(PurchaseOrderItem, PurchaseOrderItemAdmin)
