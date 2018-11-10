3c51ccc873b7978daba7fa0691699d040047f09b
kemong <kemong.dev123@gmail.com>
2018-11-10 16:48:11 +0700

    all_new

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Brand, Category, Product
+
+class BrandAdmin(admin.ModelAdmin):
+    model = Brand
+    exclude = ['slug']
 
 class CategoryAdmin(admin.ModelAdmin):
     model = Category
@@ -9,5 +13,6 @@ class ProductAdmin(admin.ModelAdmin):
     model = Product
     exclude = ['slug']
 
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
