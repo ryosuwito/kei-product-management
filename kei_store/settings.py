05e84e7966f1006d7e7314983cd2e8af73e71137
kemong <kemong.dev123@gmail.com>
2018-11-12 17:04:45 +0700

    change_template

--- kei_store/settings.py
+++ kei_store/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = '/webapps/kei/static/'
 STATICFILES_DIRS = [
     #'/home/ryo/Webproject/keskei/keskei/static/',    
     '/webapps/kei/static/',
