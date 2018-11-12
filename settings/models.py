8d146792e6b4d6e21304d03b929e98c652ad3f50
kemong <kemong.dev123@gmail.com>
2018-11-12 18:08:40 +0700

    change_template

--- settings/models.py
+++ settings/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Link(models.Model):
-    addr = models.SlugField(max_length=400)
+    addr = models.CharField(max_length=400, default='/')
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name.title()
 
 class FooterLink(models.Model):
     link = models.ManyToManyField(Link, 
@@ -15,13 +18,13 @@ class HeaderLink(models.Model):
             help_text="Link pada header")
 
 class HomeLink(models.Model):
-    addr_1 = models.SlugField(max_length=400, default='/')
-    addr_2 = models.SlugField(max_length=400, default='/')
-    addr_3 = models.SlugField(max_length=400, default='/')
-    addr_4 = models.SlugField(max_length=400, default='/')
-    addr_5 = models.SlugField(max_length=400, default='/')
-    addr_sidebar = models.SlugField(max_length=400, default='/')
-    addr_middle = models.SlugField(max_length=400, default='/')
+    addr_1 = models.CharField(max_length=400, default='/')
+    addr_2 = models.CharField(max_length=400, default='/')
+    addr_3 = models.CharField(max_length=400, default='/')
+    addr_4 = models.CharField(max_length=400, default='/')
+    addr_5 = models.CharField(max_length=400, default='/')
+    addr_sidebar = models.CharField(max_length=400, default='/')
+    addr_middle = models.CharField(max_length=400, default='/')
     banner_1 = models.ImageField(upload_to = 'home_settings', null=True,
             help_text="Banner 1")
     banner_2 = models.ImageField(upload_to = 'home_settings', null=True,
