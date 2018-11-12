17cc4206d7a1a9a53e3a718ba65245a564f74577
kemong <kemong.dev123@gmail.com>
2018-11-12 16:22:40 +0700

    change_template

--- storefront/views.py
+++ storefront/views.py
@@ -189,8 +189,12 @@ def product_by_brand(request, brand_pk, **kwargs):
 
 def product_by_price(request, start_price, end_price, **kwargs):
     try:
-        product_list = Product.objects.filter(is_archived=False, 
-                        price__range=(start_price, end_price)).order_by('price')
+        if start_price < end_price and end_price > 0:
+            product_list = Product.objects.filter(is_archived=False, 
+                            price__range=(start_price, end_price)).order_by('price')
+        else:
+            product_list = Product.objects.filter(is_archived=False, 
+                            price__gte=(start_price)).order_by('price')
     except:
         product_list=''
     if not product_list:
