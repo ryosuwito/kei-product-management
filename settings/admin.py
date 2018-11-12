e7ff3017035fa8a75f8de554a5a30e88ccd3854d
kemong <kemong.dev123@gmail.com>
2018-11-12 18:01:15 +0700

    change_template

--- settings/admin.py
+++ settings/admin.py
@@ -3,19 +3,15 @@ from .models import HeaderLink, FooterLink, HomeLink, Link
 
 class HeaderLinkAdmin(admin.ModelAdmin):
     model = HeaderLink
-    exclude = ['slug']
 
 class FooterLinkAdmin(admin.ModelAdmin):
     model = FooterLink
-    exclude = ['slug']
 
 class HomeLinkAdmin(admin.ModelAdmin):
     model = HomeLink
-    exclude = ['slug']
 
 class LinkAdmin(admin.ModelAdmin):
     model = Link
-    exclude = ['slug']
 
 admin.site.register(HeaderLink, HeaderLinkAdmin)
 admin.site.register(FooterLink, FooterLinkAdmin)
--- settings/models.py
+++ settings/models.py
@@ -16,25 +16,25 @@ class HeaderLink(models.Model):
 
 class HomeLink(models.Model):
     addr1 = models.SlugField(max_length=400)
+    addr2 = models.SlugField(max_length=400)
+    addr3 = models.SlugField(max_length=400)
+    addr4 = models.SlugField(max_length=400)
+    addr5 = models.SlugField(max_length=400)
+    addr_sidebar = models.SlugField(max_length=400)
+    addr_middle = models.SlugField(max_length=400)
     banner1 = models.ImageField(upload_to = 'home_settings',
             help_text="Banner 1")
-    addr2 = models.SlugField(max_length=400)
     banner2 = models.ImageField(upload_to = 'home_settings',
             help_text="Banner 2")
-    addr3 = models.SlugField(max_length=400)
     banner3 = models.ImageField(upload_to = 'home_settings',
             help_text="Banner 3")
-    addr4 = models.SlugField(max_length=400)
     banner1 = models.ImageField(upload_to = 'home_settings',
             help_text="Banner 4")
-    addr5 = models.SlugField(max_length=400)
     banner5 = models.ImageField(upload_to = 'home_settings',
             help_text="Banner 5")
     
-    addr_sidebar = models.SlugField(max_length=400)
     banner_sidebar = models.ImageField(upload_to = 'home_settings',
             help_text="Banner Sidebar")
 
-    addr_middle = models.SlugField(max_length=400)
     banner_middle = models.ImageField(upload_to = 'home_settings',
             help_text="Banner Middle")
