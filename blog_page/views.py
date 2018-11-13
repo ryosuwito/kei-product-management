1af9c3704ac2ee840b0542fcf2eae87b170927ee
kemong <kemong.dev123@gmail.com>
2018-11-13 15:25:52 +0700

    change_template

--- blog_page/views.py
+++ blog_page/views.py
@@ -16,9 +16,18 @@ def page(request, page_slug):
 def article(request, article_slug):
     article =  get_object_or_404(Article, slug=article_slug)
     all_product = Product.objects.filter(is_archived=False)
+    all_article = Product.objects.filter(is_archived=False).exclude(slug=article_slug)
     if len(all_product) >= 5:
         other_product = random.sample(list(all_product), 4)
     else:
         other_product = random.sample(list(all_product), len(all_product))
 
-    return render(request, 'blog_page/blog_detail.html', {"article":article, 'other_product':other_product})
+    if len(all_article) >= 4:
+        other_article = random.sample(list(all_article), 3)
+    else:
+        other_article = random.sample(list(all_article), len(all_article))
+
+    return render(request, 'blog_page/blog_detail.html', 
+            {"article":article, 
+             'other_article':other_article,
+             'other_product':other_product})
